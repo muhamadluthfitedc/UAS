3221d54e08127b95866f27968c3ab1e195c597f1
muhamadluthfitedc <muhamadluthfi1703@gmail.com>
2021-07-28 08:42:01 -0700

    wilujeng sumping

---
+++ src/components/CFooter.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-footer dark height="auto" color="secondary">
+        <v-flex xs23>
+            <v-card text tile class="white--text text-xs-center" color="secondary">
+
+                <v-card-text>
+                    <v-btn v-for="icon in icons" :key="icon" class="mx-3 white--text" icon>
+                        <v-icon size="24px">{{ icon }}</v-icon>
+                    </v-btn>
+                </v-card-text>
+
+                <v-layout justify-center row wrap>
+                    <v-btn v-for="link in links" :key="link" color="white" text rounded>
+                        {{ link }}
+                    </v-btn>
+
+                    <v-flex py-3 white--text xs12 color="primary">
+                        &copy;2021 - MLuthfi & Akmal <strong>TokoHPDjaja</strong>
+                    </v-flex>
+                </v-layout>
+
+            </v-card>
+        </v-flex>
+    </v-footer>
+</template>
+<script>
+export default {
+    data: () => ({
+        icons: [
+            'fab fa-facebook',
+            'fab fa-twitter',
+            'fab fa-google-plus',
+            'fab fa-linkedin',
+            'fab fa-instagram'
+        ],
+        links: [
+            'Home',
+            'About Us',
+            'Team',
+            'Services',
+            'Blog',
+            'Contact Us'
+        ]
+    }),
+}
+</script>
